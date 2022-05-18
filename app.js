67c3462d3679423a7abf8946e43ab63970f01c8a
Giovanni La Perna <giovannilaperna@gmail.com>
2022-05-18 13:10:20 -0300

    remove app.use allow origin

--- app.js
+++ app.js
@@ -108,12 +108,6 @@ app.use(login)
 app.use(codeSend)
 app.use(codeVerify)
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-});
-
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
