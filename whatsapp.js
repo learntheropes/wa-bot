9ed43efe5b377e1b1d5c1200c69e5537487c96e9
Giovanni La Perna <giovannilaperna@gmail.com>
2022-06-08 14:02:27 -0300

    remove fauna update of wa status

--- whatsapp.js
+++ whatsapp.js
@@ -1,6 +1,5 @@
 const { Client, LocalAuth } = require('whatsapp-web.js')
 var qrcode = require('qrcode-terminal')
-const { q, client } = require('./lib/fauna')
 
 const wa = new Client({
     puppeteer: { headless: true },
@@ -21,24 +20,12 @@ wa.on('auth_failure', msg => {
     console.error('AUTHENTICATION FAILURE', msg)
 })
 
-wa.on('ready', async () => {
+wa.on('ready', () => {
     console.log('CONNECTED')
-    await client.query(
-        q.Update(
-            q.Ref(q.Collection('status'), '332036570083229762'),
-            { data: { state: 'CONNECTED' }},
-        )
-    )
 })
 
-wa.on('change_state', async (state) => {
+wa.on('change_state', state => {
     console.log(state)
-    await client.query(
-        q.Update(
-            q.Ref(q.Collection('status'), '332036570083229762'),
-            { data: { state }},
-        )
-    )
 })
 
 wa.on('disconnected', (reason) => {
